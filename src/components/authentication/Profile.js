a2e9db4f390aacef1eaa143d6a25f1b1a522ee42
ThomasTW935 <thomastw935@gmail.com>
2021-04-29 11:16:56 +0800

    Change Profile.js style

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {useAuth} from '../../context/AuthContext'
 
 export default function Profile() {
+    const {currentUser} = useAuth()
+
     return (
-        <div>
-            Hello
+        <div className='center'>
+        <div className='formContainer'>
+            <h2>Profile</h2>
+            <div className='profile'>
+                <strong>Email:</strong>
+                <span>{currentUser.email}</span>
+            </div>
+            <Link className='btn btn-primary' to='/update-profile'>Update Profile</Link>
+            <Link to='/signup'>Log out</Link>
         </div>
+      </div>
     )
 }
