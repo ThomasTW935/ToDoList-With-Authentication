ada68e49d3abb9a2a21fba38da1814fde46e567c
ThomasTW935 <thomastw935@gmail.com>
2021-04-30 13:03:45 +0800

    Added checkbox logic

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,21 +1,45 @@
 import React, { useReducer, useState } from 'react'
 
-function reducer(todos, {type, payload}){
-
+const ACTIONS = {
+    ADD_TODO: 'add-todo',
+    UPDATE_TODO: 'update-todo',
+    DELETE_TODO: 'delete-todo',
+    UPDATE_STATUS: 'update-status'
 }
 
-const ACTIONS = {}
+
+function reducer(todos, {type,payload}){
+    console.log(type)
+    switch(type){
+        case ACTIONS.UPDATE_TODO:
+            return todos.map(todo=> {
+                console.log(payload)
+                if(todo.id === payload.id){
+                    return {...todo,  complete:!todo.complete}
+                }
+                return todo
+            })
+        default: return todos
+    }
+}
+
+
 const STATUSES = ['all','active','completed']
 
 export default function Todos() {
 
     const [todos, dispatch] = useReducer(reducer, 
         [
-            { id: 1, todo: "Run", isCompleted: false },
-            { id: 2, todo: "Swim 3x", isCompleted: true },
-            { id: 3, todo: "Code", isCompleted: false },
+            { id: 1, todo: "Run", complete: false },
+            { id: 2, todo: "Swim 3x", complete: true },
+            { id: 3, todo: "Code", complete: false },
     ])
     const [activeStatus, setActiveStatus] = useState(STATUSES[0])
+
+
+    function handleCheckBox(todo){
+        dispatch({type: ACTIONS.UPDATE_TODO, payload: {id:todo.id}})
+    }
 
     return (
         <div className='todos'>
@@ -30,18 +54,19 @@ export default function Todos() {
             <div className='todos__body'>
                 { todos.map(todo=>
                     {
-                        if(activeStatus === STATUSES[1] && todo.isCompleted) return 
-                        if(activeStatus === STATUSES[2] && !todo.isCompleted) return 
-                        return <div className='todo'>
+                        if(activeStatus === STATUSES[1] && todo.complete) return
+                        if(activeStatus === STATUSES[2] && !todo.complete) return
+                        return <div key={todo.id} className='todo'>
                             <label className='custom-checkbox'>
-                               <input checked={todo.isCompleted} className='custom-checkbox-input' type='checkbox' />
+                               <input onChange={()=> handleCheckBox(todo)} checked={todo.complete} className='custom-checkbox-input' type='checkbox' />
                                <span className='custom-checkbox-span'></span>
                             </label>
-                            <p className={todo.isCompleted ? 'todo-task todo-completed':'todo-task'}>{todo.todo}</p>
+                            <p className={todo.complete ? 'todo-task todo-completed':'todo-task'}>{todo.todo}</p>
                             <button className='btn btn-warning'>Del</button>
                         </div>
                     }
                 ) }
+               
             </div>
             <div className='todos__footer'>
                 <div>3 items left</div>
