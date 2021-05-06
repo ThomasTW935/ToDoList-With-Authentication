e29e15be99c81cf73dad654a37efab86a47b13f9
ThomasTW935 <thomastw935@gmail.com>
2021-05-06 10:20:04 +0800

    Added dark mode

---
+++ src/components/Todos/ThemeChange.js
@@ -0,0 +1,27 @@
+import React, {useState,useEffect} from 'react'
+import moon from '../../images/icon-moon.svg'
+import sun from '../../images/icon-sun.svg';
+
+export default function ThemeChange() {
+  const [darkTheme, setDarkTheme] = useState(false)
+
+  function handleThemeChange(){
+    setDarkTheme( prevTheme => !prevTheme)
+    if(darkTheme){
+      localStorage.setItem('Theme','dark')
+      document.body.classList.add('dark-mode')
+      return
+    } 
+    localStorage.setItem('Theme','light')
+    document.body.classList.remove('dark-mode')
+  }
+
+  useEffect(() => {
+    const getTheme = localStorage.getItem('Theme')
+    if(getTheme === 'dark') return document.body.classList.add('dark-mode')
+  }, [])
+
+  return (
+    <button className='btn' onClick={handleThemeChange}><img src={ darkTheme ? moon : sun } alt='Change Theme' /></button>
+  )
+}
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,60 +1,53 @@
 import React, { useReducer, useState, useRef, useEffect } from 'react'
-import moon from '../../images/icon-moon.svg'
-import sun from '../../images/icon-sun.svg';
-import bgDesktopDark from '../../images/bg-desktop-dark.jpg';
-import bgDesktopLight from '../../images/bg-desktop-light.jpg';
 import Todo from './Todo';
 import {database} from '../../firebase'
 import {useAuth} from '../../context/AuthContext'
+import ThemeChange from './ThemeChange';
 
 export const ACTIONS = {
-    ADD_TODO: 'add-todo',
-    UPDATE_TODO: 'update-todo',
-    DELETE_TODO: 'delete-todo',
-    CLEAR_TODOS: 'clear-todos',
-    SET_TODOS: 'set-todos',
-    UPDATE_STATUS: 'update-status'
+  ADD_TODO: 'add-todo',
+  UPDATE_TODO: 'update-todo',
+  DELETE_TODO: 'delete-todo',
+  CLEAR_TODOS: 'clear-todos',
+  SET_TODOS: 'set-todos',
+  UPDATE_STATUS: 'update-status'
 }
 
 
 function reducer(todos, {type,payload}){
-    switch(type){
-        case ACTIONS.SET_TODOS: 
-            return [...payload.todos]
-        case ACTIONS.ADD_TODO:
-            return [...todos, newTodo(payload.task, payload.complete)]
-        case ACTIONS.UPDATE_TODO:
-            return todos.map(todo=> {
-                console.log(payload)
-                if(todo.id === payload.id){
-                    return {...todo,  complete:!todo.complete}
-                }
-                return todo
-            })
-        case ACTIONS.DELETE_TODO:
-            return todos.filter(todo=> todo.id !== payload.id)
-        case ACTIONS.CLEAR_TODOS:
-            return []
-        default: return todos
-    }
+  switch(type){
+    case ACTIONS.SET_TODOS: 
+      return [...payload.todos]
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(payload.task, payload.complete)]
+    case ACTIONS.UPDATE_TODO:
+      return todos.map(todo=> {
+        console.log(payload)
+        if(todo.id === payload.id){
+            return {...todo,  complete:!todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo=> todo.id !== payload.id)
+    case ACTIONS.CLEAR_TODOS:
+      return []
+    default: return todos
+  }
 }
 
 function newTodo(task,complete){
-    return {id:Date.now(), task: task, complete: complete}
+  return {id:Date.now(), task: task, complete: complete}
 }
 
 const STATUSES = ['all','active','completed']
-const THEMES = {
-    LIGHT: 'light',
-    DARK: 'dark'
-}
+
 
 export default function Todos() {
 
     const [taskName,setTaskName] = useState('')
     const [taskStatus, setTaskStatus] = useState(false)
     const formRef = useRef()
-    const [theme, setTheme] = useState(THEMES.LIGHT)
     const {currentUser}= useAuth()
 
     const [todos, dispatch] = useReducer(reducer,[
@@ -64,7 +57,6 @@ export default function Todos() {
     ])
     const [activeStatus, setActiveStatus] = useState(STATUSES[0])
 
-    console.log(todos)
 
     useEffect(()=>{ 
         return database.todos.where('userId', '==', currentUser.uid)
@@ -86,54 +78,52 @@ export default function Todos() {
         })
         dispatch({type: ACTIONS.ADD_TODO, payload: {task:taskName, complete: taskStatus}})
         formRef.current.reset()
-        
     }
 
     
     function handleClear(){
         // dispatch({type: ACTIONS.CLEAR_TODOS, payload: {}})
     }
-    function handleThemeChange(){
-        setTheme( prevTheme => prevTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT )
-    }
+   
 
     function test(){}
 
     return (
         <div className='todos'>
-            <div className={ `todos__background ${theme}-theme` }></div>
+            <div className='todos__background'></div>
             <div className='todos__header'>
                 <h1>TODO</h1>
-                <button className='btn' onClick={handleThemeChange}><img src={ theme === THEMES.LIGHT ? moon : sun } /></button>
-                <form ref={formRef} onSubmit={handleForm} className='todo'>
+                <ThemeChange/>
+            </div>
+            <div className='todos__body'>
+                <form ref={formRef} onSubmit={handleForm} className='todo todos__form'>
                     <label className='custom-checkbox'><input onChange={(e)=>{ setTaskStatus( prevStatus=> !prevStatus ) }} className='custom-checkbox-input' type='checkbox' /><span className='custom-checkbox-span'></span></label>
                     <input onChange={ (e)=> { setTaskName(e.target.value) } }  className='todo-task' placeholder='Create a new Todo' />
                 </form>
-            </div>
-            <div className='todos__body'>
-                { todos.map(todo=>
-                    {
-                        if(activeStatus === STATUSES[1] && todo.complete) return
-                        if(activeStatus === STATUSES[2] && !todo.complete) return
-                        return <Todo key={todo.id} todo={todo} dispatch={test} /> 
-                    }
-                ) }
-               
-            </div>
-            <div className='todos__footer'>
-                <div>3 items left</div>
-                <section>
-                    {
-                        STATUSES.map((value,index) =>
-                            <button key={index} onClick={ ()=>{ setActiveStatus(value) } }
-                            className={value === activeStatus ? 'btn btn-status btn-status-active' : 'btn btn-status'} >{value}</button>
-                        )
+                <div className='todos__list'>
+                    { todos.map(todo=>
+                        {
+                            if(activeStatus === STATUSES[1] && todo.complete) return
+                            if(activeStatus === STATUSES[2] && !todo.complete) return
+                            return <Todo key={todo.id} todo={todo} dispatch={test} /> 
+                        }
+                    ) }
+                
+                </div>
+                <div className='todos__footer'>
+                    <div>{todos && todos.length} items left</div>
+                    <section className='todos__status'>
+                        {
+                            STATUSES.map((value,index) =>
+                                <button key={index} onClick={ ()=>{ setActiveStatus(value) } }
+                                className={value === activeStatus ? 'btn btn-status btn-status-active' : 'btn btn-status'} >{value}</button>
+                            )
 
-                    }
-                </section>
-                <button onClick={ handleClear }className='btn btn-status'>Clear Completed</button>
+                        }
+                    </section>
+                    <button onClick={ handleClear }className='btn btn-status'>Clear Completed</button>
+                </div>
             </div>
-            {currentUser.uid}
         </div>
     )
 }
