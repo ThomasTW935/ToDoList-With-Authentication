3cc66698b2f1a9e764d2fdc69b77c8c695b736c5
Daryl Thomas <thomastw935@gmail.com>
2021-10-09 07:27:59 +0800

    Remove login and signup button at navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import '../styles/main.css'
-import { AuthProvider } from '../context/AuthContext'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Signup from './authentication/Signup'
-import Profile from './authentication/Profile'
-import PrivateRoute from './authentication/PrivateRoute'
-import Login from './authentication/Login'
-import UpdateProfile from './authentication/UpdateProfile'
-import ForgotPassword from './authentication/ForgotPassword'
-import Todos from './Todos/Todos'
-import NavBar from './NavBar'
+import React from "react"
+import "../styles/main.css"
+import { AuthProvider } from "../context/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Signup from "./authentication/Signup"
+import Profile from "./authentication/Profile"
+import PrivateRoute from "./authentication/PrivateRoute"
+import Login from "./authentication/Login"
+import UpdateProfile from "./authentication/UpdateProfile"
+import ForgotPassword from "./authentication/ForgotPassword"
+import Todos from "./Todos/Todos"
+import NavBar from "./NavBar"
 
 function App() {
   return (
     <div className="App">
       <Router>
         <AuthProvider>
-          <NavBar/>
+          <NavBar />
           <Switch>
-            <PrivateRoute exact path='/' component={Todos}/>
-            <PrivateRoute path='/profile' component={Profile} />
-            <PrivateRoute path='/update-profile' component={UpdateProfile} />
-            <Route path='/forgot-password' component={ForgotPassword}/>
-            <Route path='/signup' component={Signup}/>
-            <Route path='/login' component={Login}/>
+            <PrivateRoute exact path="/" component={Todos} />
+            <PrivateRoute path="/profile" component={Profile} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
           </Switch>
         </AuthProvider>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {useAuth} from '../context/AuthContext'
+import React from "react"
+import { Link } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 export default function NavBar() {
-    const {currentUser} = useAuth()
+  const { currentUser } = useAuth()
 
-    const renderComponent=()=>{
-        if(currentUser) {
-            return <Link to='/profile' className='btn btn-primary'>Profile</Link>
-        }
-        return <>
-            <Link to='/login' className='btn btn-primary'>Login</Link>
-            <Link to='/signup' className='btn btn-neutral'>Sign Up</Link>
-        </>
-        
+  const renderComponent = () => {
+    if (currentUser) {
+      return (
+        <Link to="/profile" className="btn btn-primary">
+          Profile
+        </Link>
+      )
     }
-
     return (
-        <nav>
-            { 
-                renderComponent() 
-            }
-        </nav>
+      <>
+        {/* <Link to='/login' className='btn btn-primary'>Login</Link>
+            <Link to='/signup' className='btn btn-neutral'>Sign Up</Link> */}
+      </>
     )
+  }
+
+  return <nav>{renderComponent()}</nav>
 }
